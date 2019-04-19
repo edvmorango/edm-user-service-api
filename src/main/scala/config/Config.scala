bf6d3431b6d67f7a087ae13d322e67f19dc6f1d8
J. Eduardo V. Morango <edvmorango@gmail.com>
2019-04-19 17:15:52 -0300

    feature: config

---
+++ src/main/scala/config/Config.scala
@@ -0,0 +1,31 @@
+package config
+
+final case class Config(app: AppConfig, aws: AWSConfig)
+
+final case class AppConfig(host: String, context: String, port: Int)
+
+final case class AWSConfig(dynamodb: DynamoDBConfig)
+
+final case class DynamoDBConfig(prefix: String,
+                                region: String,
+                                host: String,
+                                port: Int,
+                                parallelism: Int,
+                                accessKey: String,
+                                secretKey: String)
+
+object ConfigLoader {
+  import java.nio.file.Path
+  import pureconfig.generic.auto._
+  import scala.util.Try
+  import pureconfig.module.yaml._
+
+  def load: Either[Throwable, Config] = {
+
+    val path = Path of ClassLoader.getSystemResource("application.yaml").getPath
+
+    Try(loadYamlsOrThrow[Config](path)).toEither
+
+  }
+
+}
